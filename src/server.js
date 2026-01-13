f203573b602e54ec2d61112511e5f98d2e28dc7d
SushantKumarSanu <sushantkumar14507@gmail.com>
2026-01-14 00:52:16 +0530

    chore: initial backend setup with server , database , and base structure

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+import http from 'http';
+import app from './app.js';
+import connectDB from './configs/db.js';
+
+
+connectDB();
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000 ;
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
