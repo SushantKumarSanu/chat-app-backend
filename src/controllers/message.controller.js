3c8803d1d13851735453913f4142b0e2289781aa
SushantKumarSanu <sushantkumar14507@gmail.com>
2026-01-15 22:45:58 +0530

    feat : add pagination to fetch messages API

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -28,16 +28,22 @@ export const sendMessage = async (req,res) =>{
 
 export const fetchMessages = async(req,res) =>{
     try{
-        const{chatId} = req.params
+        const {chatId} = req.params
         if(!chatId){
             return res.status(400).json({message:"Chatid is required"});
-        }
+        };
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 20 
+        const skip = (page - 1)*limit;
         const messages = await Message.find({chat:chatId})
         .populate("sender","username avatar email")
-        .populate("chat");
+        .populate("chat")
+        .sort({createdAt:-1})
+        .skip(skip)
+        .limit(limit)
         res.status(200).json(messages);
 
     }catch(error){
         res.status(500).json({message:"server side error"});
-    }
-}    
+    };
+};
