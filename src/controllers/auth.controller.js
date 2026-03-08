34d30086b3d1cbe05cd731645235b3c8b6afdd09
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-03-08 16:42:17 +0530

    fix(auth):return consistant user object from login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const login = async (req,res)=>{
             message:"login Successful",
             token,
             user:{
-                id:user._id,
+                _id:user._id,
                 username:user.username,
                 email:user.email
             }
