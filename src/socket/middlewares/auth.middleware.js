e6e43355ff73aa47250eb9f4e55b711ce72aaa2e
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-04 10:58:30 +0530

    refactor: improve Socket.IO authentication error handling

--- src/socket/middlewares/auth.middleware.js
+++ src/socket/middlewares/auth.middleware.js
@@ -1,22 +1,72 @@
 import jwt from 'jsonwebtoken';
 import User from '../../models/User.js';
+import logger from '../../logger/logger.js';
+import SocketError from '../errors/SocketError.js';
 
 
 
 
 const authMiddleware = async(socket,next)=>{
-    try{
+
         const token = socket.handshake.auth?.token;
         if (!token) {
-            return next(new Error("No token provided"));
+            return next(new SocketError(
+                "No token provided",
+                "AUTHENTICATION_ERROR"
+            ));
         };
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        socket.userId = decoded.userId;
-        await User.findByIdAndUpdate(socket.userId,{isOnline:true});
+
+        let decoded ;
+        
+        try {
+
+            decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+            socket.userId = decoded.userId;
+
+            const user = await User.findByIdAndUpdate(socket.userId,{isOnline:true});
+
+            
+            if(!user){
+                return next( new SocketError("User not found",
+                    "USER_NOT_FOUND"
+                ));
+            };
+
+
+
+        } catch (error) {
+
+            if (error.name === "JsonWebTokenError"){
+                return next( new SocketError(
+                    "Invalid token" ,
+                    "AUTHENTICATION_ERROR"
+                    ));
+            };
+
+            if(error.name === "TokenExpiredError"){
+                return next( new SocketError(
+                    "Token Expired",
+                    "AUTHENTICATION_ERROR"
+                ));
+            };
+
+            logger.error(
+                {
+                err: error,
+                socketId: socket.id
+                },
+                "Unhandled socket authentication error"
+            );
+
+            return next(new Error("Something went wrong")); 
+
+        }
+        
+        
+
         next();
-    }catch{
-        next(new Error("Not authenticated")); 
-    };
+
 
 };
 
