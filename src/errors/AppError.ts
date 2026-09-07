95928d1ce138a0121b95b4dd5f8a2022ce58cbf0
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-07 17:44:39 +0530

    refactor: migrate AppError to TypeScript

---
+++ src/errors/AppError.ts
@@ -0,0 +1,9 @@
+class AppError extends Error {
+    constructor(message:string, public statusCode:number){
+        super(message);
+
+        this.name = "AppError";
+    }
+};
+
+export default AppError;
